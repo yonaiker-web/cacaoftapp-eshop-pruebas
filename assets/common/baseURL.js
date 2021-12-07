9e0e9c23af39771711f79a9679b7341ea0af79c4
yonaiker-web <53097119+yonaiker-web@users.noreply.github.com>
2021-12-07 09:17:34 -0400

    Update baseURL.js

--- assets/common/baseURL.js
+++ assets/common/baseURL.js
@@ -1,7 +1,7 @@
 //archivo que conecta la app con la base de datos en la ruta espesificada
 import { Platform } from "react-native";
 
-let baseURL = 'https://cacapftapp-backend.herokuapp.com/api/v1/';
+let baseURL = 'url';
 
 /* {Platform.OS == 'android'
 ? baseURL = 'http://192.168.8.101:3010/api/v1/'
